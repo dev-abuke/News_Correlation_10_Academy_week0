e37e466417ce72c3d29c9a401f993decca10bf6d
dev-abuke <abubekershamil123@gmail.com>
2024-04-08 18:33:02 +0300

    Update SlackDataLoader to NewsDataLoader

--- src/loader.py
+++ src/loader.py
@@ -11,7 +11,7 @@ from time import sleep
 
 
 # Create wrapper classes for using slack_sdk in place of slacker
-class SlackDataLoader:
+class NewsDataLoader:
     '''
     Slack exported data IO class.
 
