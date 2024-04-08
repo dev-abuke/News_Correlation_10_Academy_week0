c5e5d587b19421ca277b30c4a2f9edde06ffe0e3
dev-abuke <abubekershamil123@gmail.com>
2024-04-08 21:41:09 +0300

    Update NewsEDA.ipynb and config.py

--- src/config.py
+++ src/config.py
@@ -6,16 +6,14 @@ default_path = os.path.join("data")
 default_news_path = os.path.join(default_path, 'rating.csv')
 
 parser = argparse.ArgumentParser(description='cmdArgs')
-parser.add_argument('--output', type=str, default='news_ouput.csv',
+parser.add_argument('--output', type=str, default='rating.csv',
                 help='filename to write analysis output in CSV format')
-parser.add_argument('--path', required=False, type=str, default=default_news_path, help='directory where news data reside')
+parser.add_argument('--path', required=False, type=str, default=default_path, help='directory where slack data reside')
 parser.add_argument('--channel', type=str, default='', help='which channel we parsing')
 parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
 
-cfg = parser.parse_args()
+cfg, unknown_args = parser.parse_known_args()
 # print(cfg)
-
-
 
 # Print the values of the arguments
 print(f'Output File: {cfg.output}')
