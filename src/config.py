db6dd8a8fec9b61fa082a124c4a0ebfaa1a18c49
dev-abuke <abubekershamil123@gmail.com>
2024-04-08 21:00:49 +0300

    Update config.py with new default values and print argument values

--- src/config.py
+++ src/config.py
@@ -1,12 +1,24 @@
 from __future__ import print_function
 import argparse
+import os
+
+default_path = os.path.join("data")
+default_news_path = os.path.join(default_path, 'rating.csv')
 
 parser = argparse.ArgumentParser(description='cmdArgs')
-parser.add_argument('--output', type=str, default='slack_data.csv',
+parser.add_argument('--output', type=str, default='news_ouput.csv',
                 help='filename to write analysis output in CSV format')
-parser.add_argument('--path', required=True, type=str, help='directory where slack data reside')
+parser.add_argument('--path', required=False, type=str, default=default_news_path, help='directory where news data reside')
 parser.add_argument('--channel', type=str, default='', help='which channel we parsing')
 parser.add_argument('--userfile', type=str, default='users.json', help='users profile information')
 
 cfg = parser.parse_args()
 # print(cfg)
+
+
+
+# Print the values of the arguments
+print(f'Output File: {cfg.output}')
+print(f'Path: {cfg.path}')
+print(f'Channel: {cfg.channel}')
+print(f'Userfile: {cfg.userfile}')
